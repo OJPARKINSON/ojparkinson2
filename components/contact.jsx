5a6725c3ce7786cf8d46cd2860e4ce9df0147be7
OJPARKINSON <olliepar01@gmail.com>
2020-08-12 21:31:00 +0100

    added linting

---
+++ components/contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = () => (
+  <ul className="contactContainer">
+    <ContactPod text="Linkedin" link="https://www.linkedin.com/in/oliver-parkinson-a551b9166" smallText="Work experiance" />
+    <ContactPod text="Instagram" link="https://www.instagram.com/olliecodes/" smallText="Day to day" />
+    <ContactPod text="Github" link="https://github.com/ojparkinson" smallText="Current projects" />
+    <ContactPod text="Medium" link="https://medium.com/@olliepar01" smallText="Blog" />
+  </ul>
+);
+
+const ContactPod = ({ link, text, smallText }) => (
+  <li className="contactItem">
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <h3>{text}</h3>
+      <p>{smallText}</p>
+    </a>
+  </li>
+);
+
+export default Contact;
---
+++ components/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Oliver Parkinson</h1>
+    <ul>
+      <li>
+        <a href="/#Portfolio">PORTFOLIO</a>
+      </li>
+      <li>
+        <a href="/">BLOG</a>
+      </li>
+      <li>
+        <a href="/#About">ABOUT</a>
+      </li>
+      <li>
+        <a href="/#Contact">CONTACT</a>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Header;
